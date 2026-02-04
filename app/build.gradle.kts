9580b7a7bfdf014220c384ac1333e5176b9ccf4c
Ilya <kuilef42@gmail.com>
2026-02-04 14:53:59 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.anotepad"
         minSdk = 29
         targetSdk = 35
-        versionCode = 14
-        versionName = "2.0"
+        versionCode = 15
+        versionName = "2.1"
     }
 
     val releaseSigning = if (hasKeystoreProperties) {
