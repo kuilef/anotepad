5c26fe8a371526e77869fe10bd751f60ebe75e6a
Ilya <kuilef42@gmail.com>
2026-02-04 14:44:41 +0200

    Add WindowInsets ime import

--- app/src/main/java/com/anotepad/ui/EditorScreen.kt
+++ app/src/main/java/com/anotepad/ui/EditorScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
