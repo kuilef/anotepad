3a9361b4fd57e160d0d200547a3dfe71a98cafc8
Ilya <kuilef42@gmail.com>
2026-02-06 20:31:15 +0200

    Remove obsolete WindowInsets getTop/getBottom imports in BrowserScreen

--- app/src/main/java/com/anotepad/ui/BrowserScreen.kt
+++ app/src/main/java/com/anotepad/ui/BrowserScreen.kt
@@ -13,8 +13,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.getBottom
-import androidx.compose.foundation.layout.getTop
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
