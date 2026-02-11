ed14336f0e666947038762ba46653934c2b9a2e8
Kuznetsov Ilya <kuilef42@gmail.com>
2026-02-11 22:21:14 +0200

    top up verson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.anotepad"
         minSdk = 29
         targetSdk = 35
-        versionCode = 21
-        versionName = "2.5.1"
+        versionCode = 22
+        versionName = "2.5.2"
     }
 
     val releaseSigning = if (hasKeystoreProperties) {
